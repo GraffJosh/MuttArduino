cfb017d4305b46ba054b0224adbd5377a6d1f8ee
graffjosh <github@joshuagraff.com>
2016-11-19 12:32:21 -0500

    added member functions for Trajectory; Untested

--- lib/Trajectory.h
+++ lib/Trajectory.h
@@ -5,25 +5,30 @@
 
 
 #define TRAJ_LENGTH 25
+#define SOLENOID_TOP 70 //angle after which to use solenoid
+#define SOLENOID_STEP 15 //angles per time to trigger solenoid
+
 class Leg;
 
-struct Frame {
+typedef struct _Frame {
   Leg *leg1;
   double leg_pos;
   bool solenoid;
-};
+} Frame;
 
 class Trajectory
 {
 private:
-  int curr_time=0,curr_index=0;
+  int curr_time,curr_index;
   Frame sequence[TRAJ_LENGTH];
 public:
   Trajectory();
-  int send_trajectory();
-  void push_back(Leg *leg1, double leg_pos, bool solenoid);
-  void push_back(Frame *new_frame, int new_time);
+  int send_trajectory(int curr_time);
+	Frame* get_frame(int curr_time);
+  int push_back(Leg *leg1, double leg_pos, bool solenoid);
+  int push_back(Frame *new_frame, int num_frames);
   int insert_frame(Leg *leg1, double leg_pos, bool solenoid, int new_time);
+	int plan_motion(Leg *leg, double start_angle, double end_angle, int time_period);
 
 };
 
--- src/Trajectory.cpp
+++ src/Trajectory.cpp
@@ -3,29 +3,52 @@
 
 Trajectory::Trajectory(void)
 {
-
+	curr_index = 0;
+	curr_time = 0;
 }
 
-int Trajectory::send_trajectory()
+
+//adds trajectory to the list of trajectories to be executed
+int Trajectory::send_trajectory(int curr_time)
 {
+	Frame *curr_frame = get_frame(curr_time);
+	curr_frame->leg1->set_position(curr_frame->leg_pos);
+	curr_frame->leg1->set_solenoid(curr_frame->solenoid);
 
   return curr_time;
 }
 
-void Trajectory::push_back(Frame *new_frame, int new_time)
+Frame* Trajectory::get_frame(int time_step)
 {
-
-
+	return sequence+(time_step*sizeof(Frame));
 }
 
-void Trajectory::push_back(Leg *leg1, double leg_pos, bool solenoid)
+//add a frame to the end of a Trajectory/
+//stub
+int Trajectory::push_back(Frame *new_frame, int num_frames)
 {
-  Frame *new_frame = sequence+(curr_index*sizeof(Frame));
-  new_frame->leg1 = leg1;
-  new_frame->leg_pos = leg_pos;
-  new_frame->solenoid = solenoid;
+	return -1;
 }
 
+
+//add a frame to the end of a trajectory
+int Trajectory::push_back(Leg *leg1, double leg_pos, bool solenoid)
+{
+	if(curr_index<TRAJ_LENGTH)
+	{
+	  Frame *new_frame = sequence+(curr_index*sizeof(Frame));
+	  new_frame->leg1 = leg1;
+	  new_frame->leg_pos = leg_pos;
+	  new_frame->solenoid = solenoid;
+		++curr_index;
+		return curr_index;
+	}else{
+		return -1;
+	}
+}
+
+
+//inserts a frame into the middle of a trajectory
 int Trajectory::insert_frame(Leg *leg1, double leg_pos, bool solenoid, int new_time)
 {
   if(new_time<=curr_index)
@@ -37,4 +60,24 @@ int Trajectory::insert_frame(Leg *leg1, double leg_pos, bool solenoid, int new_t
   }
 
   return new_time;
+}
+
+
+//appends a motion to the trajectory
+int Trajectory::plan_motion(Leg *leg, double start_angle, double end_angle, int time_period)
+{
+	int step=0,angle=0;
+	bool solenoid;
+	if(curr_index+time_period < TRAJ_LENGTH)
+	{
+		step = (end_angle-start_angle)/time_period;
+		angle = start_angle;
+		for(int i=0;i<time_period;i++)
+		{
+			solenoid = (angle > SOLENOID_TOP)&&(step>SOLENOID_STEP) ? true:false;
+			push_back(leg, start_angle+step,solenoid);
+			angle += step;
+		}
+	}
+	return curr_index;
 }
