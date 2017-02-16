a735fe7f904ba252f56699ebbdaec4bbb44fcd4d
SwagMasterAlonso <almartnz@gmail.com>
2017-02-16 09:53:46 -0500

    Adding frame.h header

---
+++ lib/Frame.h
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#ifndef frame_h
+#define frame_h
+
+class Frame {
+    
+    private:
+        int upper_pos_rb;
+        int upper_pos_lb;
+        int upper_pos_lf;
+        int upper_pos_rf;
+        
+        
+        
+        int lower_pos_rb;
+        int lower_pos_lb;
+        int lower_pos_lf;
+        int lower_pos_rf;
+    
+    
+    public:
+       
+};
+
+#endif
