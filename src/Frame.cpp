a7d2ad577e869ab6492faf392e0b65dcc24b1c1b
SwagMasterAlonso <almartnz@gmail.com>
2017-02-16 10:01:12 -0500

    Adding frame.cpp

---
+++ src/Frame.cpp
@@ -0,0 +1,19 @@
+#include "../lib/Frame.h"
+
+
+Frame::Frame(int upper_pos_rb_var, int upper_pos_lb_var, int upper_pos_lf_var, int upper_pos_rf_var, int lower_pos_rb_var,
+        int lower_pos_lb_var,
+        int lower_pos_lf_var,
+        int lower_pos_rf_var){
+    
+    int upper_pos_rb = upper_pos_rb_var;
+    int upper_pos_lb = upper_pos_lb_var;
+    int upper_pos_lf = upper_pos_lf_var;
+    int upper_pos_rf = upper_pos_rf_var;
+     
+    int lower_pos_rb = lower_pos_rb_var;
+    int lower_pos_lb = lower_pos_lb_var;
+    int lower_pos_lf = lower_pos_lf_var;
+    int lower_pos_rf = lower_pos_rf_var;
+    
+}
